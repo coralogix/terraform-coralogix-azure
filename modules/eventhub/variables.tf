bacdcbaf1474bcddcf8dc3778724b2d57f33b7eb
Michael Briggs <michael.briggs@coralogix.com>
2023-04-04 14:42:51 -0400

    Updated Terraform modules and documentation New StorageQueue integration

--- modules/eventhub/variables.tf
+++ modules/eventhub/variables.tf
@@ -1,8 +1,8 @@
 variable "CoralogixRegion" {
   description = "The Coralogix location region, possible options are [Europe, Europe2, India, Singapore, US]"
   type        = string
-    validation {
-    condition = contains(["Europe","Europe2","India","Singapore","US"], var.CoralogixRegion)
+  validation {
+    condition     = contains(["Europe", "Europe2", "India", "Singapore", "US"], var.CoralogixRegion)
     error_message = "The coralogix region must be on of these values: [Europe, Europe2, India, Singapore, US]."
   }
 }
@@ -25,25 +25,25 @@ variable "CoralogixSubsystem" {
 
 variable "FunctionResourceGroupName" {
   description = "The name of the resource group into which to deploy the Function App"
-  type = string
+  type        = string
 }
 
 variable "FunctionStorageAccountName" {
   description = "The name of the storage account that the Function App will use"
-  type = string
+  type        = string
 }
 
 variable "EventhubResourceGroupName" {
   description = "The name of the resource group that the eventhub belong to"
-  type = string
+  type        = string
 }
 
 variable "EventhubNamespace" {
   description = "The name of the EventHub Namespace."
-  type = string
+  type        = string
 }
 
 variable "EventhubInstanceName" {
   description = "The name of the EventHub Instance."
-  type = string
+  type        = string
 }
