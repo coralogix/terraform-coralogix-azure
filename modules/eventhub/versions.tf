d217cfcb4bbeefc7efbf6bd5f47091c4817d1233
vjofusha <vjosa.fusha@coralogix.com>
2025-11-20 13:36:16 +0100

    ci: add terraform validation workflow

--- modules/eventhub/versions.tf
+++ modules/eventhub/versions.tf
@@ -7,7 +7,7 @@ terraform {
       version = "~> 3.93"
     }
     random = {
-      source               = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "~> 3.4.3"
     }
   }
